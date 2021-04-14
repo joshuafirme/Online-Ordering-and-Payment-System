1c632ed3253df2f9a58028fd6ab417a2833ee3cf
dyl4nxd <dyl4nxd@gmail.com>
2021-04-14 19:19:46 +0800

    Update footer.blade.php

--- resources/views/customer/layouts/footer.blade.php
+++ resources/views/customer/layouts/footer.blade.php
@@ -1,9 +1,4 @@
- <!---=====Social Icons=====-->
- <section id="social-icons">
-    <a href="https://www.facebook.com/DavidsGrillbyBe4" target="_blank"><i class="fab fa-facebook facebook"></i></a>
-    <a href="https://www.instagram.com/explore/locations/105198337731884/davids-grill/" target="_blank"><i class="fab fa-instagram instagram"></i></a>
-</section>
-<!--=====End of Social Icons======-->
+
 
 <!---=====Footer========-->
 <footer class="footer">
