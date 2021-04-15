8bbbee24140d3c47bb6aad0578e303c24e6ae759
josh <61042416+joshuafirme@users.noreply.github.com>
2021-04-15 15:53:12 +0800

    changed color

--- resources/views/customer/js/cart.blade.php
+++ resources/views/customer/js/cart.blade.php
@@ -24,7 +24,7 @@
         </div>
         <div class="modal-footer">
           <a class="btn btn-sm btn-primary" href="{{ url('/cart') }}">Go to Cart<a>
-          <button class="btn btn-sm btn-outline-success" data-dismiss="modal">Continue Shopping</button>
+          <button class="btn btn-sm btn-outline-light"  style="color:#005B96;" data-dismiss="modal">Continue Shopping</button>
         </div>
       </div>
     </div>
