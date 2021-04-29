76b7c8e0a3550b549cc8755bd4745b5a5ec8dd5e
josh <61042416+joshuafirme@users.noreply.github.com>
2021-04-29 10:14:12 +0800

    update sidebar for cashier and receptionist

--- resources/views/layouts/side_menu.blade.php
+++ resources/views/layouts/side_menu.blade.php
@@ -88,13 +88,29 @@
     @elseif($position=='Cashier' || $position=='Receptionist')
     <li class="treeview">
         <a href="#">
-            <i class="fa fa-dollar"></i>
-            <span>Transaction</span>
+            <i class="fa fa-bar-chart-o"></i>
+            <span>Transaction <span class="badge badge-success">{{ \Helper::countDeliveryNotif()!=0 ? \Helper::countDeliveryNotif() : "" }}</span></span>
             <i class="fa fa-angle-left pull-right"></i>
         </a>
         <ul class="treeview-menu">
-            <li><a href="{{ url('transaction/cashiering') }}"><i class="fa fa-angle-double-right"></i> Cashiering</a></li>
-            <li><a href="{{ url('transaction/delivery') }}"><i class="fa fa-angle-double-right"></i> Delivery</a></li>
+            <li><a href="{{ url('transaction/cashiering') }}"><i class="fa fa-dollar"></i> Cashiering</a></li>
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-truck"></i>
+                    <span>Delivery</span>
+                    <i class="fa fa-angle-left pull-right"></i>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href="{{ url('delivery/pending') }}"><i class="fa fa-angle-double-right"></i> Pending 
+                        <span class="badge badge-success">{{ \Helper::countPending()!=0 ? \Helper::countPending() : "" }}</span></a></li>
+                    <li><a href="{{ url('delivery/preparing') }}"><i class="fa fa-angle-double-right"></i> Preparing 
+                        <span class="badge badge-success">{{ \Helper::countPreparing()!=0 ? \Helper::countPreparing() : "" }}</span></a></li>
+                    <li><a href="{{ url('delivery/dispatch') }}"><i class="fa fa-angle-double-right"></i> Dispatch 
+                        <span class="badge badge-success">{{ \Helper::countDispatch()!=0 ? \Helper::countDispatch() : "" }}</span></a></li>
+                    <li><a href="{{ url('delivery/delivered') }}"><i class="fa  fa-angle-double-right"></i> Delivered</a></li>
+                    <li><a href="{{ url('delivery/cancelled') }}"><i class="fa fa-angle-double-right"></i> Cancelled</a></li>
+                </ul>
+            </li>
         </ul>
     </li>
     @endif
